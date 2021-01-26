75432f9e6af548bcb01d430d980ba8b592153e03
Sunday <sugars_tang@163.com>
2021-01-26 15:36:18 +0000

    切割头部和尾部子视图

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,5 +5,8 @@
     <h1>Hello Laravel</h1>
     <p class="lead">这是一个Laravel示例项目主页</p>
     <p>一切将从这里开始</p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
   </div>
 @stop
