711f0b3e5fbe3a548bdcfd772a62f9ccb6a5558f
Sunday <sugars_tang@163.com>
2021-02-02 13:32:31 +0000

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,15 +76,15 @@ class User extends Authenticatable
 
     public function follow($user_ids)
     {
-        if (!is_array($user_ids)) {
+        if ( ! is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
-        $this->followings()->sync($user_ids, flase);
+        $this->followings()->sync($user_ids, false);
     }
 
     public function unfollow($user_ids)
     {
-        if (!is_array($user_ids)) {
+        if ( ! is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->detach($user_ids);
